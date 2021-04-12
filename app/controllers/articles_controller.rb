623f6a533ce029b98bc90c8deeab519934846d3c
Code <mac92j@pm.me>
2021-04-12 19:30:00 -0400

    Add edit and update actions to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,5 +24,14 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was successfully edited"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+end
