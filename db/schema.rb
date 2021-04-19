bbcac160658624b1fe0eb43eb7a2abe370213421
Code <mac92j@pm.me>
2021-04-19 18:56:52 -0400

    Add article_categories tables and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_000737) do
+ActiveRecord::Schema.define(version: 2021_04_19_224238) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
